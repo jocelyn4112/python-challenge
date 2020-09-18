546b44eb30a011897b99bbbc0fde138f47e6b37c
jocelyn4112 <68340393+jocelyn4112@users.noreply.github.com>
2020-09-18 10:30:29 -0400

    PyPoll Canidate Work

--- PyPoll2/main.py
+++ PyPoll2/main.py
@@ -1,9 +1,9 @@
+# Import Dependencies 
 import os
 import csv
 import statistics 
-
-output_path = os.path.join("Resources", "election_data.csv")
 # open csv file
+output_path = os.path.join("Resources", "election_data.csv")
 with open(output_path) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     print(csvreader)
@@ -12,45 +12,48 @@ with open(output_path) as csvfile:
     print(f"CSV Header: {csv_header}")
  #Blank Lists and variables 
     total_votes = 0
-    # List of canidates 
     total_candidate_list = []
-    # Number of votes each canidate recieves 
-    candidate_votes = []
-    Percent_Votes = []
+    candidate_votes = 0
+    Percent_Votes = 0
 #for total votes cast 
 for Total_votes in csvreader:
      Total =+ 1
-    #Compiling total canidates and votes - if candidate is not on the list of canidates they and their votes are added on.  
+#Compiling total canidates and votes - if candidate is not on the list of canidates they and their votes are added on.  
 if row[2] not in total_candidate_list:      
         total_candidate_list.append.row[2]
         index = total_candidate_list.index(row[2])
         total_votes.append(1)
-else:
+ else:
         index = total_candidate_list.index(row[2])
         total_votes[index] += 1
-    print(total_candidate_list)
-    print(total_votes)
-    #Make Index Canidate List
+
+#Make Index Canidate List
     candidateIndex = candidates.index(row[2])
-     # Index to count of votes for the candidate
+# Index to count of votes for the candidate
     total_votes = [candidateIndex] += 1
-    #percentage each caniadite won 
-        #Sum each cannidates votes - Citation?
-       # for votes in total_candidates:
-        #    percentage = (votes/total_votes) * 100
-         #   percentage = round(percentage)
-          #  percentage = "%.3f%%" % percentage
-           # percent_votes.append(percentage)
-        #Divide by total votes
-               # /Total_votes
-    #Total muber for votes for each canidate 
+#Total muber for votes for each canidate 
     for row in csvreader:
         if row[0] == candidate:
             Total =+ 1
-         #Sum Canidates A,B,C
-    #winner
+#percentage each caniadite won 
+    for votes in total_candidates:
+        percentage = (votes/total_votes) * 100
+        percentage = round(percentage)
+        percentage = "%.3f%%" % percentage
+        percent_votes.append(percentage)
+        Divide by total votes
    
+   #This is how the tutor suggested I figure this which make smore sense but was only covered in the class before, so I didn't know if I could use it in the homework. 
+    #khan_lit = list.loc[candidate  == "khan"]
+    #correy_lit = list.loc[candidate  == "correy"] 
+    #Li_lit = list.loc[candidate  == "li"]
+
     
+    sum
+    #winner     
+   
+ print(total_candidate_list)
+print(total_votes)   
 print(Total)
 
 
