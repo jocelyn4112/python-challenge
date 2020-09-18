ccf581783b242a3216c80bcf54068633ee4daca5
jocelyn4112 <68340393+jocelyn4112@users.noreply.github.com>
2020-09-18 13:43:44 -0400

    PyPoll and Bank cleaning

--- PyBank2/main.py
+++ PyBank2/main.py
@@ -4,43 +4,47 @@ import csv
 import statistics 
 
 #Open CSV Files
-output_path = os.path.join("Resources", "../budget_data.csv")
+output_path = os.path.join("Resources","budget_data.csv")
 # open csv file
 with open(output_path) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    print(csvreader)
-# if printing the header
-    #csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")   
+    csvreader = csv.reader(csvfile)
+ 
+    #for row in csvreader:
+    #    print(row)
+
+    # if printing the header
+    csv_header = next(csvreader)
+    print("CSV Header:", csv_header)   
 # start with blank lists  - How to make sure it is pulling data?
     Date = []
     Rev1 = []
     Rev2 = []
-    Total = 0
+    total = 0
     Delta = []
-#For number of months - total length of months 
-Total_Months = len(csvreader)
-
-#Total Revenue - sum of revenu
-for Rev1 in csvreader:  
+#For number of months = total length of months (points back to csv file  
+    Total_Months = len("Months")
+    #print(Total_Months)
+#Total Revenue - sum of revenue
+    for Rev1 in csvreader:  
          total += 1
 #or
-Total = sum(Rev1)
-print(total)   
+#Total = sum(Delta)
+    #print(total)   
 
 # Average Change - calcualte the delta/month and average.    
-for "row2" in csvreader: #this needs ot be named something that refefnces somethwer e- not sure where
+for i,row2 in enumerate(csvreader): #this needs ot be named something that refefnces somethwer e- not sure where
+    if i == 0:
+        continue 
     Rev1 = []
     Rev2 = []
-    Delta = [Rev2[i]-Rev1[i] for i in xrange(min(len(Rev1), len(Rev2)))]
-    
+    Delta = [Rev2[row[1]]-Rev1[i] for i in xrange(min(len(Rev1), len(Rev2)))] 
     #print (Delta)
- average = statistics.mean(Delta)
-      #or
-Average = Delta / Total_Months      
-#Greatest increase - max / month
+average = statistics.mean(Delta)
+        #or
+        # Average = Delta / Total_Months      
+    #Greatest increase - max / month
 max_change = max(Delta)
-#Greatest Decrease - min / month
+    #Greatest Decrease - min / month
 min_change = min(Delta)
     
 
--- PyPoll2/main.py
+++ PyPoll2/main.py
@@ -12,54 +12,57 @@ with open(output_path) as csvfile:
     print(f"CSV Header: {csv_header}")
  #Blank Lists and variables 
     total_votes = 0
+    canidates = []
     total_candidate_list = []
     candidate_votes = 0
     percent_votes = 0
-#for total votes cast 
-for Total_votes in csvreader:
-     Total =+ 1
-#Compiling total canidates and votes - if candidate is not on the list of canidates they and their votes are added on.  
-if row[2] not in total_candidate_list:      
-        total_candidate_list.append.row[2]
-        index = total_candidate_list.index(row[2])
-        total_votes.append(1)
-else:
-        index = total_candidate_list.index(row[2])
-        total_votes[index] += 1
+    canidates_info = {}
+   # Creating candidate list
+    for i, row in enumerate (csvreader):
+        if i == 0: 
+            continue
+        candidate_name = row[2]     
+        if candidate_name in canidates_info.keys():
+            canidates_info[candidate_name] += 1
+        else:
+            canidates_info[candidate_name] = 1
+        total_votes = 0
+    #for total votes cast by candidate 
+    for candidate in canidates_info.keys():
+        total_votes += canidates_info[candidate] 
 
-#Make Index Canidate List
-    candidateIndex = candidates.index(row[2])
-# Index to count of votes for the candidate
-    total_votes = [candidateIndex] += 1
-#Total muber for votes for each canidate 
-    for row in csvreader:
-        if row[0] == candidate:
-            Total =+ 1
 #percentage each caniadite won 
-    for votes in total_candidates:
+    for canidate in canidates_info.keys():
+        votes = canidates_info[candidate]
         percentage = (votes/total_votes) * 100
         percentage = round(percentage)
         percentage = "%.%%" 
-        percent_votes.append(percentage)
+        canidates_info[canidate] = {"Votes":votes, "Percentage":percentage}
       
    
    #This is how the tutor suggested I figure this which make smore sense but was only covered in the class before, so I didn't know if I could use it in the homework. 
     #khan_lit = list.loc[candidate  == "khan"]
     #correy_lit = list.loc[candidate  == "correy"] 
     #Li_lit = list.loc[candidate  == "li"]
-
-   for winner in candidate_votes   
-     if vote_count > winner_votes:
-        winner = vote_count
-        winner = 
+Max_vote = 0
+Winner = None
+for canidates in canidates_info.keys(): 
+    if candidate_info[canidates]["votes"] > Max_vote:
+        Winner = canidates
+        Max_vote = candidate_info[canidates]["votes"]
         
-   
-print(total_candidate_list)
-print(total_votes) 
+print ('Election Results')   
+print(__________________________)
+print(candidates_info.keys())
+print(total_votes)
+for canidates in canidates_info.keys():
+    print(f'{canidates}: {candidates_info[canidates]["percentage"]} {candidate_info[candidates]["votes"]}')
+
 print (total_votes-canidates)  
 print(percent_votes)
 print(Total)
 print(winner_votes)
+print(winner)
 
-
-   
+with open('Results.txt', 'a+') as f:
+  f.write(ff)  
